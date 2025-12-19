dc8974a332ac1575074b9e5c457ca45ab7fb7f6f
Nabila Syahri <nnabilasyahri@gmail.com>
2025-12-19 11:37:45 +0700

    perubahan backend

--- server/index.js
+++ server/index.js
@@ -8,14 +8,18 @@ const app = express();
 // Port dinamis untuk Railway, kalau di lokal pakai 5000
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(bodyParser.json());
 
 // KONFIGURASI DATABASE (pakai DATABASE_URL dari railway)
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false // untuk neon
+    rejectUnauthorized: false // WAJIB ADA agar neon mau terima koneksi dari Railway
   }
 });
 
